054350cd9313ccf7efbcb9173999e0c05e6efc9e
unknown <501818616@CORP-66Q3XY1.logon.ds.ge.com>
2014-08-05 21:03:07 -0400

    Sets paths to local variables

--- cookbooks/railo/recipes/default.rb
+++ cookbooks/railo/recipes/default.rb
@@ -5,19 +5,24 @@
 
 require "fileutils"
 
+railo_path_temp = "/tmp/railo/railo-express"
+railo_path = node[:railo_path]}
+railo_version = node[:railo_version]
+webroot = node[:webroot]
+
 # create the path for railo
-if File.exist?("#{node[:railo_path]}")
-	FileUtils::remove_dir "#{node[:railo_path]}"
+if File.exist?(railo_path)
+	FileUtils::remove_dir railo_path
 end
-if File.exist?("/tmp/railo/railo-express")
-	FileUtils::remove_dir "/tmp/railo/railo-express"
+if File.exist?(railo_path_temp)
+	FileUtils::remove_dir railo_path_temp
 end
-FileUtils::mkdir_p "#{node[:railo_path]}"
-FileUtils::mkdir_p "/tmp/railo/railo-express"
+FileUtils::mkdir_p railo_path
+FileUtils::mkdir_p railp_path_temp
 
 # download Railo Express (includes Jetty)
-remote_file "/tmp/railo/railo-express/railo-express-#{node[:railo_version]}-jre-linux64.tar.gz" do
-	source "http://www.getrailo.org/railo/remote/download42/#{node[:railo_version]}/railix/linux/railo-express-#{node[:railo_version]}-jre-linux64.tar.gz"
+remote_file "#{railo_path_temp}/railo-express-#{railo_version}-jre-linux64.tar.gz" do
+	source "http://www.getrailo.org/railo/remote/download42/#{railo_version}/railix/linux/railo-express-#{railo_version}-jre-linux64.tar.gz"
 	action :create_if_missing
 	mode "0744"
 	owner "root"
@@ -25,33 +30,34 @@ remote_file "/tmp/railo/railo-express/railo-express-#{node[:railo_version]}-jre-
 end
 
 # extract the files
-execute "tar xvzf railo-express-#{node[:railo_version]}-jre-linux64.tar.gz" do
-  creates "railo-#{node[:railo_version]}"
+execute "tar xvzf railo-express-#{railo_version}-jre-linux64.tar.gz" do
+  creates "railo-#{railo_version}"
   action :run
   user "root"
-  cwd "/tmp/railo/railo-express"
+  cwd railo_path_temp
 end
 
 # move everything to the railo path
-execute "cp -R ./ #{node[:railo_path]}/" do
+execute "cp -R ./ #{railo_path}/" do
 	action :run
 	user "root"
-	cwd "/tmp/railo/railo-express/railo-express-#{node[:railo_version]}-jre-linux64"
+	cwd "#{railo_path_temp}/railo-express-#{railo_version}-jre-linux64"
 end
 
 # modify the railo webroot path
+# TODO: set the webroot to the shared code folder
 
 # copy the index and init setup files
-template "#{node[:webroot]}/index.cfm" do
+template "#{webroot}/index.cfm" do
   source "index.cfm.erb"
   action :create_if_missing
 end
 
-template "#{node[:webroot]}/_cfinit.cfm" do
+template "#{webroot}/_cfinit.cfm" do
   source "_cfinit.cfm.erb"
 end
 
-execute "chown -R root:root #{node[:railo_path]}" do
+execute "chown -R root:root #{railo_path}" do
 	action :run
 	user "root"
 end
